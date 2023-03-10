c61ebb7ce5ebbca535f06f8693a05422e87a1568
Raja Sekhar <rajasekhar.b237@gmail.com>
2023-03-10 22:43:01 +0530

    Changed CSS

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -10,15 +10,18 @@ const Banner = () => {
   return (
     <div className="hero-section__container">
       <div className="banner-bg">
-        <Container className="hero-section">
-          <h1 className="hero-section__text">
-            Reading is an adventure, and we&apos;ve got your ticket.
-          </h1>
-          <p className="hero-section__subtext">
-            We present you to the world of books where you can find the best at
-            affordable rates, so escape reality and find your books with us.
-          </p>
-          <Button text="Coming soon!" className="mt-16" />
+        <Container>
+          <div className="hero-section">
+            <h1 className="hero-section__text">
+              Reading is an adventure, and we&apos;ve got your ticket.
+            </h1>
+            <p className="hero-section__subtext">
+              We present you to the world of books where you can find the best
+              at affordable rates, so escape reality and find your books with
+              us.
+            </p>
+            <Button text="Coming soon!" className="mt-16" />
+          </div>
         </Container>
       </div>
     </div>
